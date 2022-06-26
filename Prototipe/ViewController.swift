d2e71f56f14ce4b2052393177ba56e6742507650
r0zari0 <maksymstovolos2002@gmail.com>
2022-06-26 15:55:40 -0400

    HW2

--- Prototipe/ViewController.swift
+++ Prototipe/ViewController.swift
@@ -7,13 +7,58 @@
 
 import UIKit
 
+enum CurrentLight{
+    case red
+    case yellow
+    case green
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLigth: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    
+    @IBOutlet weak var startButton: UIButton!
+     
+    private var currentLight = CurrentLight.yellow
+    private let lightIsOff: CGFloat = 0.3
+    private let lightisOn: CGFloat = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startButton.layer.cornerRadius = 10
+        
+        redLight.alpha = lightisOn
+        yellowLigth.alpha = lightIsOff
+        greenLight.alpha = lightIsOff
+       
+        setUpView()
     }
-
-
-}
+    
+    func setUpView() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLigth.layer.cornerRadius = yellowLigth.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+    }
+ 
+    @IBAction func buttonAction(_ sender: Any) {
+        
+        switch currentLight {
+        case .red:
+            redLight.alpha = lightisOn
+            greenLight.alpha = lightIsOff
+            currentLight = .yellow
+        case .yellow:
+            redLight.alpha = lightIsOff
+            yellowLigth.alpha = lightisOn
+            currentLight = .green
+        case .green:
+            yellowLigth.alpha = lightIsOff
+            greenLight.alpha = lightisOn
+            currentLight = .red
+        }
+    }
+        
+    }
 
